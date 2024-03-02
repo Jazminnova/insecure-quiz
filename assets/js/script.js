60cf2ec33ba594ff7b2344c86b24bba050f71e09
jazminnova <jasminnova@gmail.com>
2024-03-01 21:08:53 -0500

    added script and made eddits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-//start button
-var startbtn = document.querySelector("#startbtn");
+// DOM VARIABLES
+var heading = document.querySelector("header");
+var quizstrt = document.querySelector("#startquiz");
+var timer = document.querySelector("#timer");
+var instructions = document.querySelector("#directions");
+var quizContainer = document.querySelector(".quizContainer");
+var askQuestions = document.querySelector("#questions");
+var submit = document.querySelector("#submit");
 
-function quizstrt() {
-  window.location.href = "./assets/html/questions.html";
-}
-
-startbtn.addEventListener("click", quizstrt);
+// variables
+let time = 90;
+let questionIndex = 0;
---
+++ assets/js/startbutton.js
@@ -0,0 +1,8 @@
+//start button
+var starttbtn = document.querySelector("#starttbtn");
+
+function quizstrt() {
+  window.location.href = "./questions.html";
+}
+
+startbtn.addEventListener("click", quizstrt);
