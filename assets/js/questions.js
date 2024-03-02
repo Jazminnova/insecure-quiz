1ebd9cbc468eb9bae6acc735067b77cce57dd95c
jazminnova <jasminnova@gmail.com>
2024-03-01 20:42:25 -0500

    added quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,43 @@
+// Quiz Questions
+const questions = [
+  {
+    q: "Who is Issa's Bestfriend ?",
+    a: ["Molly", "Daniel", "Kelly", "Condola"],
+    c: "Molly",
+  },
+  {
+    q: "What is the profession of the main character, Issa Dee, in the show? ",
+    a: ["Lawyer", "Teacher", "Realtor", "Non-profit Worker"],
+    c: "Non-profit Worker",
+  },
+  {
+    q: "What is the name of Issa's on-and-off boyfriend throughout the series?",
+    a: ["Lawrence", "Daniel", "Nathan", "Dro"],
+    c: "Lawrence",
+  },
+  {
+    q: "Which city does Insecure primarily take place in?",
+    a: ["New York City", "Los Angeles", "Atlanta", "Chicago"],
+    c: "Los Angeles",
+  },
+  {
+    q: "What is the name of the organization where Issa works in the first season?",
+    a: [
+      "We Got Y'all",
+      "Black Girls Rock",
+      "Black Lives Matter",
+      "The BeeHive",
+    ],
+    c: "We Got Y'all",
+  },
+  {
+    q: "Who did Lawrence have a baby with ?",
+    a: ["Issa", "Condola", "Tasha", "Aperna"],
+    c: "Condola",
+  },
+  {
+    q: "What is the name of the annual music and arts festival that the characters attend in Season 2?",
+    a: ["Lollapalooza", "Afropunk", "Coachella", "Burning Man"],
+    c: "Coachella",
+  },
+];
